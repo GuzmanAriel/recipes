90ee95468518cdf793e13191025d5033db9144f5
GuzmanAriel <guzmanariel525@gmail.com>
2024-12-16 19:55:29 -0600

    added in the fetch to the API

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '68cba2fba6msh9f5fdc2e429bba0p1bc93ajsn542f247a478b',
+		'x-rapidapi-host': 'tasty.p.rapidapi.com'
+	}
+};
+
+// Define an async function
+async function fetchRecipeData() {
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json(); // Change text() to json() for structured data
+		console.log(result);
+	} catch (error) {
+		console.error("Error fetching data:", error);
+	}
+}
+
+// Call the async function
+fetchRecipeData();
